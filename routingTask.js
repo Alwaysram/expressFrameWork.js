d15fb1f35717f2d0a4890d3329f1e555dd70c70d
Alwaysram <rc4663128@gmail.com>
2023-05-30 20:42:33 +0530

    middleWare

---
+++ routingTask.js
@@ -0,0 +1,34 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    const url=req.url;
+    if(url==='/home'){
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Welcome home</h1></body>');
+    res.write('</html>');
+    res.end();
+    }
+    else if(url==='/about'){
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Welcome to about us</h1></body>');
+    res.write('</html>');
+    res.end();
+    }
+    else if(url==='/node'){
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Welcome to my node js project</h1></body>');
+    res.write('</html>');
+    res.end();
+    }
+    else{
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>nothing</h1></body>');
+    res.write('</html>');
+    res.end();
+    }
+});
+server.listen(4000);

4366b0c4d5fd339c220bc7d7aa1b9f2dfa1bc10e
Alwaysram <rc4663128@gmail.com>
2023-05-30 20:39:59 +0530

    middleWare

---
+++ MiddleWare.js
@@ -0,0 +1,30 @@
+// const http=require('http');
+// const server=http.createServer((req,res)=>{
+//     console.log('naresh');
+//     process.exit();
+
+// });
+// server.listen(4000);
+
+
+// //task-1////////express///////
+// //const http=require('http');
+
+// const express = require('express');
+
+// const app= express();
+
+// app.use((req,res,next)=>{
+//     console.log('In the middle ware!');
+//     next();
+// });
+// app.use((req,res,next)=>{
+//     console.log('In the another middle ware!');
+//     res.send('<h1>Hello from express</h1>')
+// });
+
+// // const server=http.createServer(app);
+
+// // server.listen(4000);
+// app.listen(4000);
+

6df3ce6bb69cc02ced002d87623d1a709a6220de
Alwaysram <rc4663128@gmail.com>
2023-05-31 14:08:04 +0530

    routers and filters

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-//task-2////////express///////
+//task-3////////express///////
 const express = require('express');
 const bodyParser=require('body-parser');
 
 const app= express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<h1><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
-
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from express</h1>')
-});
-
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 app.listen(4000);
